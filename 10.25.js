6e5bd40e3c503b7c065d54d40cb285ec67275d0b
soohyan9 <133744562+soohyan9@users.noreply.github.com>
2023-10-25 16:40:04 +0900

    Update 10.25.js

--- 10.25.js
+++ 10.25.js
@@ -33,3 +33,56 @@ console.log(fruits);
 // pop: remove an item from the end
 fruits.pop();
 console.log(fruits);
+
+// unshift: add an item to the benigging
+fruits.unshift('딸기','레몬');
+console.log(fruits);
+
+//shift: remove an item from the begging
+// note!! shift, unshift are slower than pop, push
+//splice: remove an item by index position
+fruits.push('/사과', '/복숭아', '레몬');
+console.log(fruits);
+fruits.splice(1, 1);
+console.log(fruits);
+fruits.splice(1, 1, '/청사과', '/수박',)
+console.log(fruits);
+
+//combine two arrays
+const fruits2 = ['/서양배', '/코코넛'];
+const newFruits = fruits.concat(fruits2);
+console.log(newFruits);
+
+// 5. Searching
+// IndexOf: find the index
+console.clear();
+console.log(fruits);
+console.log(fruits.indexOf('/사과'));
+console.log(fruits.indexOf('/수박'));
+console.log(fruits.indexOf('/코코넛'));
+
+//includes
+console.log(fruits.includes('/수박'));
+console.log(fruits.includes('/코코넛'));
+
+// lasIndexOf
+fruits.push('/사과');
+console.log(fruits);
+console.log(fruits.indexOf('/사과'));
+console.log(fruits.lastIndexOf('/사과'));
+
+//9강 배열함수
+
+//Q1.make a string out of an array
+{
+    const fruits = ['apple', 'banana', 'orange'];
+    const result = fruits.join(',');
+    console.log(result);
+}
+
+//Q2. make an array out of a string
+{
+    const fruits = '/사과, /키위, /바나나, /체리';
+    const result = fruits.split(',');
+    console.log(result);
+}
